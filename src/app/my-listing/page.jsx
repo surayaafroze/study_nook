3f35bcd9c0c68c85d510cdebb8e9d97d23b3ff66
surayaafroze63 <surayaafroze63@gmail.com>
2026-05-22 17:03:02 +0600

    add my listing page

--- src/app/my-listing/page.jsx
+++ src/app/my-listing/page.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-
-const MyListingspage = () => {
-  return (
-    <div>
-      my slisdkalglhj
-    </div>
-  );
-};
-
-export default MyListingspage;
+"use client";
+ 
+import Image from "next/image";
+import React, { useState, useEffect } from "react";
+import { useSession } from "@/lib/auth-client"; // adjust path
+import {
+  FiEdit2,
+  FiTrash2,
+  FiAlertTriangle,
+  FiPlus,
+  FiMapPin,
+  FiUsers,
+  FiDollarSign,
+  FiX,
+  FiCheck,
+} from "react-icons/fi";
+import toast from "react-hot-toast";
+import Link from "next/link";
